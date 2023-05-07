e8ae0ef210e2dec869f1a58582301745367b5aef
EKar <Earthb0y@outlook.com>
2023-05-07 15:51:11 +0800

    refactor: Isolate TOKEN and redesign the route generation method

--- src/pages/[workId].json.ts
+++ src/pages/[workId].json.ts
@@ -1,37 +1,28 @@
 import type { APIRoute } from 'astro'
 const url = 'https://api.vika.cn/fusion/v1/datasheets/dstgK4fhao4Qv6Ztfr/records'
-const token = 'Bearer uskK5FftsbXnLqrOV5DjzLY'
+const token = `Bearer ${import.meta.env.IMAGE_HOST_TOKEN}`
 const work = await fetch(url, {
   headers: {
     Authorization: token,
   },
 })
 const workData = await work.json()
-const workRecords = workData.data.records.map((i) => {
-  return i.fields
-})
+const workRecords = workData.data.records.map(i => i.fields)
 
+const workIndex: Record<string, number> = {
+  tu: 0,
+  un: 1,
+  al: 2,
+  ne: 3,
+  all: 5,
+}
 export const get: APIRoute = async({ params }) => {
-  let work
-  switch (params.workId) {
-    case 'tu':
-      work = workRecords[0]
-      break
-    case 'un':
-      work = workRecords[1]
-      break
-    case 'al':
-      work = workRecords[2]
-      break
-    case 'ne':
-      work = workRecords[3]
-      break
-    case 'all':
-      work = workRecords.map((work) => {
-        return { workName: work.workName, Cover: work.Cover }
-      })
-      break
-  }
+  const index = workIndex[params.workId!]
+  const work = index !== undefined
+    ? index === 5
+      ? workRecords.map(({ workName, Cover }) => ({ workName, Cover }))
+      : workRecords[index]
+    : null
   return {
     body: JSON.stringify(work),
   }
