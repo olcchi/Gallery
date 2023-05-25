3e9eebf7f7c6144e25bacd287a85a37dd77d6c27
EKar <Earthb0y@outlook.com>
2023-05-25 23:16:58 +0800

    refactor: Extract function

---
+++ src/scripts/_getWorks.ts
@@ -0,0 +1,10 @@
+type works = 'all' | 'tu' | 'al' | 'un'
+export async function getWorks(workName: works, _href) {
+  const href = import.meta.env.PROD
+    ? 'https://ekargallery.netlify.app/'
+    : _href
+  const url = workName === 'all' ? `${href}all.json` : `${href}/${workName}.json`
+  const res = await fetch(url)
+  const _workList = await res.json()
+  return _workList
+}
